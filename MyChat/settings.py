ff4bd38d2f6fd4801cda770ae125fe1b89116096
Ritik Pandey <ritik.py@gmail.com>
2023-02-07 17:57:08 +0530

    code update

--- MyChat/settings.py
+++ MyChat/settings.py
@@ -151,13 +151,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
 ]
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-
-TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
 BASE_URL = "http://127.0.0.1:8000"
 
